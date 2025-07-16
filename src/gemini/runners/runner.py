6af0156199010ed3f30d9dd7007c39099b482812
Pedro J. Aguayo <pedro.aguayo@blueskydataplatform.com>
2025-07-15 23:00:17 -0400

    Creates the initial SDK using the Google ADK

---
+++ src/gemini/runners/runner.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Any
+from google.genai import types as genai_types
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from gemini.agents.llm_agent import GeminiLLMAgent
+
+
+class GeminiRunner(Runner):
+    def __init__(self, agent: GeminiLLMAgent, name: str, session_id: str):
+        self.name = name
+        self.agent = agent
+        self.session_service = None
+        self.runner = None
+        self.session_id = session_id
+    async def initialize(self):
+        self.session_service = InMemorySessionService()
+        await self.session_service.create_session(
+            user_id=self.name,
+            app_name=self.name,
+            session_id=self.session_id,
+        )
+        super().__init__(
+            app_name=self.name,
+            agent=self.agent,
+            session_service=self.session_service,
+        )
+
+    async def run_async(self, query: str):
+        user_content = genai_types.Content(role='user', parts=[genai_types.Part(text=query)])
+        final_response_content = "No final response received."
+        async for event in super().run_async(user_id=self.name, session_id=self.session_id, new_message=user_content):
+            logging.debug(f"Event: {event}")
+            if event.is_final_response() and event.content and event.content.parts:
+                final_response_content = event.content.parts[0].text
+        return final_response_content
