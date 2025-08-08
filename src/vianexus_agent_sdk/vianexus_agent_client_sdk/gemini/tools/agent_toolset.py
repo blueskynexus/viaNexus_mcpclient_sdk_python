ae4d1d207ddcd04780bd40a6f492a5db43fa7fc5
Pedro J. Aguayo <pedro.aguayo@blueskydataplatform.com>
2025-08-08 12:36:51 -0400

    Removing stale directories

--- src/vianexus_agent_sdk/vianexus_agent_client_sdk/gemini/tools/agent_toolset.py
+++
@@ -1,9 +0,0 @@
-from google.adk.tools.agent_tool.agent_toolset import AgentToolset
-from google.adk.tools.agent_tool.agent_toolset import StreamableHTTPConnectionParams
-
-
-class GeminiAgentToolset(AgentToolset):
-    def __init__(self, connection_params: StreamableHTTPConnectionParams):
-        super().__init__(
-            connection_params=connection_params
-        )
